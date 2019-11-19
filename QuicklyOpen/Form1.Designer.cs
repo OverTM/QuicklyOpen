01395b74870d547997ddc1df58cd15313ea913e4
cyoukon <1162289133@qq.com>
2019-11-19 23:29:21 +0900

    完成路径设置和快速打开功能

--- QuicklyOpen/Form1.Designer.cs
+++ QuicklyOpen/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -40,115 +41,223 @@
             this.button10 = new System.Windows.Forms.Button();
             this.button11 = new System.Windows.Forms.Button();
             this.button12 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip3 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip4 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip5 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip6 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip7 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip8 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip9 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip10 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip11 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip12 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.SuspendLayout();
             // 
             // button1
             // 
+            this.button1.ContextMenuStrip = this.contextMenuStrip1;
             this.button1.Location = new System.Drawing.Point(0, 0);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(146, 46);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
+            this.button2.ContextMenuStrip = this.contextMenuStrip2;
             this.button2.Location = new System.Drawing.Point(150, 0);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(146, 46);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
+            this.button3.ContextMenuStrip = this.contextMenuStrip3;
             this.button3.Location = new System.Drawing.Point(300, 0);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(146, 46);
             this.button3.TabIndex = 2;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
+            this.button4.ContextMenuStrip = this.contextMenuStrip4;
             this.button4.Location = new System.Drawing.Point(0, 50);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(146, 46);
             this.button4.TabIndex = 3;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
+            this.button5.ContextMenuStrip = this.contextMenuStrip5;
             this.button5.Location = new System.Drawing.Point(150, 50);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(146, 46);
             this.button5.TabIndex = 4;
             this.button5.Text = "button5";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
+            this.button6.ContextMenuStrip = this.contextMenuStrip6;
             this.button6.Location = new System.Drawing.Point(300, 50);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(146, 46);
             this.button6.TabIndex = 5;
             this.button6.Text = "button6";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
+            this.button7.ContextMenuStrip = this.contextMenuStrip7;
             this.button7.Location = new System.Drawing.Point(0, 100);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(146, 46);
             this.button7.TabIndex = 6;
             this.button7.Text = "button7";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
+            this.button8.ContextMenuStrip = this.contextMenuStrip8;
             this.button8.Location = new System.Drawing.Point(150, 100);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(146, 46);
             this.button8.TabIndex = 7;
             this.button8.Text = "button8";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
+            this.button9.ContextMenuStrip = this.contextMenuStrip9;
             this.button9.Location = new System.Drawing.Point(300, 100);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(146, 46);
             this.button9.TabIndex = 8;
             this.button9.Text = "button9";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button10
             // 
+            this.button10.ContextMenuStrip = this.contextMenuStrip10;
             this.button10.Location = new System.Drawing.Point(0, 150);
             this.button10.Name = "button10";
             this.button10.Size = new System.Drawing.Size(146, 46);
             this.button10.TabIndex = 9;
             this.button10.Text = "button10";
             this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // button11
             // 
+            this.button11.ContextMenuStrip = this.contextMenuStrip11;
             this.button11.Location = new System.Drawing.Point(150, 150);
             this.button11.Name = "button11";
             this.button11.Size = new System.Drawing.Size(146, 46);
             this.button11.TabIndex = 10;
             this.button11.Text = "button11";
             this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
             // 
             // button12
             // 
+            this.button12.ContextMenuStrip = this.contextMenuStrip12;
             this.button12.Location = new System.Drawing.Point(300, 150);
             this.button12.Name = "button12";
             this.button12.Size = new System.Drawing.Size(146, 46);
             this.button12.TabIndex = 11;
             this.button12.Text = "button12";
             this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip2.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip2_Opening);
+            // 
+            // contextMenuStrip3
+            // 
+            this.contextMenuStrip3.Name = "contextMenuStrip3";
+            this.contextMenuStrip3.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip3.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip3_Opening);
+            // 
+            // contextMenuStrip4
+            // 
+            this.contextMenuStrip4.Name = "contextMenuStrip4";
+            this.contextMenuStrip4.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip4.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip4_Opening);
+            // 
+            // contextMenuStrip5
+            // 
+            this.contextMenuStrip5.Name = "contextMenuStrip5";
+            this.contextMenuStrip5.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip5.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip5_Opening);
+            // 
+            // contextMenuStrip6
+            // 
+            this.contextMenuStrip6.Name = "contextMenuStrip6";
+            this.contextMenuStrip6.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip6.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip6_Opening);
+            // 
+            // contextMenuStrip7
+            // 
+            this.contextMenuStrip7.Name = "contextMenuStrip7";
+            this.contextMenuStrip7.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip7.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip7_Opening);
+            // 
+            // contextMenuStrip8
+            // 
+            this.contextMenuStrip8.Name = "contextMenuStrip8";
+            this.contextMenuStrip8.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip8.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip8_Opening);
+            // 
+            // contextMenuStrip9
+            // 
+            this.contextMenuStrip9.Name = "contextMenuStrip9";
+            this.contextMenuStrip9.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip9.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip9_Opening);
+            // 
+            // contextMenuStrip10
+            // 
+            this.contextMenuStrip10.Name = "contextMenuStrip10";
+            this.contextMenuStrip10.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip10.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip10_Opening);
+            // 
+            // contextMenuStrip11
+            // 
+            this.contextMenuStrip11.Name = "contextMenuStrip11";
+            this.contextMenuStrip11.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip11.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip11_Opening);
+            // 
+            // contextMenuStrip12
+            // 
+            this.contextMenuStrip12.Name = "contextMenuStrip12";
+            this.contextMenuStrip12.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip12.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip12_Opening);
             // 
             // Form1
             // 
@@ -187,6 +296,18 @@
         private System.Windows.Forms.Button button10;
         private System.Windows.Forms.Button button11;
         private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip3;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip4;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip5;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip6;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip7;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip8;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip9;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip10;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip11;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip12;
     }
 }
 
--- QuicklyOpen/Form1.cs
+++ QuicklyOpen/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;//打开外部程序用
 
 namespace QuicklyOpen
 {
@@ -16,5 +17,218 @@ namespace QuicklyOpen
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// 路径变量
+        /// </summary>
+        string path, path1, path2, path3, path4, path5, path6, path7, path8, path9, path10, path11, path12,
+               filename, filename1, filename2, filename3, filename4, filename5, filename6,
+               filename7, filename8, filename9, filename10, filename11, filename12;
+
+        #region button事件设置（左键）
+        private void ErrorCheck(string path0)
+        {
+            if ((path0 != null) && (path0 != ""))
+            {
+                Process.Start(path0);
+            }
+            else
+            {
+                MessageBox.Show("您还未设置路径");
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ErrorCheck(path1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ErrorCheck(path2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ErrorCheck(path3);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ErrorCheck(path4);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            ErrorCheck(path5);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            ErrorCheck(path6);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ErrorCheck(path7);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            ErrorCheck(path8);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            ErrorCheck(path9);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            ErrorCheck(path10);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            ErrorCheck(path11);
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            ErrorCheck(path12);
+        }
+        #endregion
+
+        #region button绑定的contextMenuStrip事件设置（鼠标右键）
+        private string PathSetting(string path, string filename, Button button, string hotkey)
+        {
+            path = Selselection() ?? path;//加判断防止打开选择路径对话框，未选择路径，而导致之前选择的路径被空值覆盖
+            //path1 = @path1.Replace(@"\", "/"); //加@防止字符串中的转义字符被处理；替换字符串中的字符。
+            path = (path != null) ? @path.Replace(@"\", "/") : null;//加判断防止path1为空时报错
+            string[] filename0 = @path.Split('/'); //将字符串按‘/’分割成数组
+            filename = filename0[filename0.Length - 1]; //取数组的最后一位
+            button.Text = hotkey + filename;
+            return filename;
+        }
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+            filename1 = PathSetting(path1, filename1, button1, "①");
+            path1 = path;
+        }
+
+        private void contextMenuStrip2_Opening(object sender, CancelEventArgs e)
+        {
+            filename2 = PathSetting(path2, filename2, button2, "②");
+            path2 = path;
+        }
+
+        private void contextMenuStrip3_Opening(object sender, CancelEventArgs e)
+        {
+            filename3 = PathSetting(path3, filename3, button3, "③");
+            path3 = path;
+        }
+
+        private void contextMenuStrip4_Opening(object sender, CancelEventArgs e)
+        {
+            filename4 = PathSetting(path4, filename4, button4, "④");
+            path4 = path;
+        }
+
+        private void contextMenuStrip5_Opening(object sender, CancelEventArgs e)
+        {
+            filename5 = PathSetting(path5, filename5, button5, "⑤");
+            path5 = path;
+        }
+
+        private void contextMenuStrip6_Opening(object sender, CancelEventArgs e)
+        {
+            filename6 = PathSetting(path6, filename6, button6, "⑥");
+            path6 = path;
+        }
+
+        private void contextMenuStrip7_Opening(object sender, CancelEventArgs e)
+        {
+            filename7 = PathSetting(path7, filename7, button7, "⑦");
+            path7 = path;
+        }
+
+        private void contextMenuStrip8_Opening(object sender, CancelEventArgs e)
+        {
+            filename8 = PathSetting(path8, filename8, button8, "⑧");
+            path8 = path;
+        }
+
+        private void contextMenuStrip9_Opening(object sender, CancelEventArgs e)
+        {
+            filename9 = PathSetting(path9, filename9, button9, "⑨");
+            path9 = path;
+        }
+
+        private void contextMenuStrip10_Opening(object sender, CancelEventArgs e)
+        {
+            filename10 = PathSetting(path10, filename10, button10, "Ⓐ");
+            path10 = path;
+        }
+
+        private void contextMenuStrip11_Opening(object sender, CancelEventArgs e)
+        {
+            filename11 = PathSetting(path11, filename11, button11, "Ⓑ");
+            path11 = path;
+        }
+
+        private void contextMenuStrip12_Opening(object sender, CancelEventArgs e)
+        {
+            filename12 = PathSetting(path12, filename12, button12, "Ⓒ");
+            path12 = path;
+        }
+        #endregion
+
+        #region 设置文件或文件夹路径
+        private string Selselection()
+        {
+            //弹出消息框，并判断选择的是“是”还是“否”
+            DialogResult ret = MessageBox.Show("是：设定文件路径\n否：设定文件夹路径", "请选择设定文件路径还是文件夹路径", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information);
+            if (ret == DialogResult.Yes)
+            {
+                OpenFileDialog fileDialog = new OpenFileDialog();//选择文件
+                fileDialog.Multiselect = true; //该值确定是否可以选择多个文件
+                fileDialog.Title = "请选择文件";
+                fileDialog.Filter = "所有文件(*.*)|*.*";
+                /*
+                 * Filter 属性 赋值为一字符串 用于过滤文件类型;
+                 *字符串说明如下：
+                 *‘|’分割的两个，一个是注释，一个是真的Filter，显示出来的是那个注释。如果要一次显示多中类型的文件，用分号分开。
+                 *如：
+                 *Open1.Filter="图片文件(*.jpg,*.gif,*.bmp)|*.jpg;*.gif;*.bmp";
+                 *则过滤的文件类型为 “|”号  右边的 *.jpg;*.gif;*.bmp 三种类型文件，在OpenDialog/SaveDialog中显示给用户看的文件类型字符串则是 ：“|”号  左边的 图片文件(*.jpg,*.gif,*.bmp)。
+                 *再如：
+                 *Open1.Filter="图像文件(*.jpg;*.jpg;*.jpeg;*.gif;*.png)|*.jpg;*.jpeg;*.gif;*.png";
+                 */
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string file = fileDialog.FileName;
+                    path = file;
+                    //MessageBox.Show("已选择文件:" + file, "选择文件提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else if (ret == DialogResult.No)
+            {
+                FolderBrowserDialog dialog = new FolderBrowserDialog();//选择文件夹
+                dialog.Description = "请选择文件夹路径";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    string foldPath = dialog.SelectedPath;
+                    path = foldPath;
+                    //MessageBox.Show("已选择文件夹:" + foldPath, "选择文件夹提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                //
+            }
+            return path;
+        }
+        #endregion
     }
 }
