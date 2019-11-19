e2640502fe171556ebc617ab3dec92c9a2d8aeac
cyoukon <1162289133@qq.com>
2019-11-19 22:54:06 +0900

    默认管理员身份运行

--- QuicklyOpen/Program.cs
+++ QuicklyOpen/Program.cs
@@ -12,11 +12,42 @@ namespace QuicklyOpen
         /// 应用程序的主入口点。
         /// </summary>
         [STAThread]
-        static void Main()
+        #region 将其中Main方法中的代码替换为以下代码即可实现程序默认以管理员身份运行。
+        static void Main(string[] Args)
         {
+            /**
+             * 当前用户是管理员的时候，直接启动应用程序
+             * 如果不是管理员，则使用启动对象启动程序，以确保使用管理员身份运行
+             */
+            //获得当前登录的Windows用户标示
+            System.Security.Principal.WindowsIdentity identity = System.Security.Principal.WindowsIdentity.GetCurrent();
+            //创建Windows用户主题
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            System.Security.Principal.WindowsPrincipal principal = new System.Security.Principal.WindowsPrincipal(identity);
+            //判断当前登录用户是否为管理员
+            if (principal.IsInRole(System.Security.Principal.WindowsBuiltInRole.Administrator))
+            {
+                //如果是管理员，则判断该程序是否已经在运行，防止重复运行
+                Application.EnableVisualStyles();
+                Application.Run(new Form1());
+            }
+            else
+            {
+                //创建启动对象
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                //设置运行文件
+                startInfo.FileName = System.Windows.Forms.Application.ExecutablePath;
+                //设置启动参数
+                startInfo.Arguments = String.Join(" ", Args);
+                //设置启动动作,确保以管理员身份运行
+                startInfo.Verb = "runas";
+                //如果不是管理员，则启动UAC
+                System.Diagnostics.Process.Start(startInfo);
+                //退出
+                System.Windows.Forms.Application.Exit();
+            }
         }
+        #endregion
     }
 }
